4f57dd2904966e532f01cbc1235dfac498e1aa2d
hadywijaya12 <hadywijaya171103@gmail.com>
2023-10-17 22:36:46 +0700

    Update kirim data komponen
    mengirim data ke komponen

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  constructor(private renderer: Renderer2){
+    this.renderer.removeClass(document.body,"login-page");
+    this.renderer.addClass(document.body,"layout-fixed");
+  }
+  ngOnInit(): void {
+      
+  }
+  nim="212102381";
+  nama="Hady Wijaya";
 }
